313e4ec77e01297673ef5aae90289da91d5a7361
Vishal Tayade <tayadevishal00@gmail.com>
2024-09-27 16:40:51 +0530

    booleans

---
+++ Javascript-Basics/2.Booleans/4.Convert Number to Boolean.js
@@ -0,0 +1,12 @@
+// Here, we declare the variable num and assign it the value 5. We also declare the variable bool which we assign the boolean representation of num.
+// Extend the code such that the console.log() statement logs false.
+
+// let num = 5;
+
+// const bool = Boolean(num);
+// console.log(bool);
+
+let num = 5;
+num = 0;
+const bool = Boolean(num);
+console.log(bool); // false
